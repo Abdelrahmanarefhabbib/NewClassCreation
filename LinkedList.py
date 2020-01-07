5a2b3eba5318c89e086fdc8cf9de5cafefb0edcc
c_rkishn <theroni92@gmail.com>
2020-01-07 17:28:26 +0200

    Finished LinkedList class

--- LinkedList.py
+++ LinkedList.py
@@ -15,28 +15,45 @@ class LinkedList:
             current_node = current_node.next_value
         current_node.next_value = Node(new_data)
 
-    def remove_node(self, remove_key):
+    def remove_node_by_key(self, remove_key):
+        if self.root_value is None:
+            return
+
         current_node = self.root_value
-        if current_node is not None:
-            if current_node.data == remove_key:
-                self.root_value = current_node.next_value
-                current_node = None
-                return
+        if current_node.data_value == remove_key:
+            self.root_value = current_node.next_value
+            return
 
         while current_node is not None:
-            if current_node.data == remove_key:
+            if current_node.data_value == remove_key:
                 break
             prev_node = current_node
             current_node = current_node.next_value
 
-        if current_node is None:
-            return
-
         prev_node.next_value = current_node.next_value
-        current_node = None
 
     def print_list(self):
         print_value = self.root_value
         while print_value is not None:
             print (print_value.data_value)
             print_value = print_value.next_value
+
+    def remove_node_by_index(self, remove_index):
+        current_node = self.root_value
+        if remove_index == 0:
+            self.root_value = current_node.next_value
+            return
+
+        for index in range(remove_index):
+            if current_node is None:
+                return
+            prev_node = current_node
+            current_node = current_node.next_value
+        prev_node.next_value = current_node.next_value
+
+    def remove_last_node(self):
+        current_node = self.root_value
+        while current_node.next_value is not None:
+            prev_node = current_node
+            current_node = current_node.next_value
+        prev_node.next_value = None
--- Main.py
+++ Main.py
@@ -2,6 +2,11 @@ from LinkedList import LinkedList
 
 
 myLinkedList = LinkedList()
-myLinkedList.add_node(2)
+myLinkedList.add_node(0)
 myLinkedList.add_node(1)
+myLinkedList.add_node(2)
+myLinkedList.add_node(3)
+myLinkedList.print_list()
+print
+myLinkedList.remove_node_by_index()
 myLinkedList.print_list()
