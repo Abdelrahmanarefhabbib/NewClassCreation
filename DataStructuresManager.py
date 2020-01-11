12edb3af63a253e704b8f1d9b702594f6a0f5118
RoniKish <R_k@gmail.com>
2020-01-11 16:12:43 +0200

    Finished MyStack

--- DataStructuresManager.py
+++ DataStructuresManager.py
@@ -1,9 +1,13 @@
 from MyLinkedList import MyLinkedList
+from MyQueue import MyQueue
+from MyStack import MyStack
 
 
 class DataStructuresManager:
     def __init__(self):
         self.linked_lists = []
+        self.queues = []
+        self.stacks = []
 
     def create_new_linked_list(self):
         self.linked_lists.append(MyLinkedList())
@@ -22,3 +26,29 @@ class DataStructuresManager:
 
     def delete_all_linked_lists(self):
         self.linked_lists = []
+
+    def create_new_queue(self, max_size=float("inf")):
+        self.queues.append(MyQueue(max_size))
+
+    def delete_queue(self, index=None):
+        """ if no index is provided last queue will be deleted"""
+        if index is None:
+            self.queues.pop()
+        else:
+            self.queues.pop(index)
+
+    def delete_all_queues(self):
+        self.queues = []
+
+    def create_new_stack(self, max_size=float("inf")):
+        self.queues.append(MyStack(max_size))
+
+    def delete_stack(self, index=None):
+        """ if no index is provided last queue will be deleted"""
+        if index is None:
+            self.stacks.pop()
+        else:
+            self.stacks.pop(index)
+
+    def delete_all_stacks(self):
+        self.stacks = []
--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 from DataStructuresManager import DataStructuresManager
 
 myManager = DataStructuresManager()
-myManager.create_new_linked_list()
-myManager.create_new_linked_list()
-myManager.linked_lists[0].add_node(1)
-myManager.linked_lists[0].add_node(2)
-myManager.linked_lists[0].add_node(3)
-myManager.print_all_linked_lists()
+myManager.create_new_stack(3)
+# myManager.stacks[0].enqueue(1)
+# myManager.stacks[0].enqueue(2)
+# myManager.stacks[0].enqueue(3)
+# print myManager.stacks[0].is_full()
+
+
