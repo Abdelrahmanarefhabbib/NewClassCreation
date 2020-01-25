2b57c784edda9d7a972326618bc66a4b2d7f3244
RoniKish <R_k@gmail.com>
2020-01-25 09:00:21 +0200

    Started AVL tree delete

--- Main.py
+++ Main.py
@@ -9,6 +9,7 @@ myManager.avl_trees[0].insert(10)
 myManager.avl_trees[0].insert(3)
 myManager.avl_trees[0].insert(2)
 myManager.avl_trees[0].insert(4)
+myManager.avl_trees[0].delete(4)
 
 
 myManager.print_all_avl_trees()
--- MyAVLTree.py
+++ MyAVLTree.py
@@ -24,21 +24,69 @@ class MyAVLTree:
 
         # Left Left
         if balance_factor > 1 and new_data < node.left.data_value:
-            return rightRotate(node)
+            return right_rotate(node)
 
         # Left Right
         if balance_factor > 1 and new_data > node.left.data_value:
-            node.left = leftRotate(node.left)
-            return rightRotate(node)
+            node.left = left_rotate(node.left)
+            return right_rotate(node)
 
         # Right Right
         if balance_factor < -1 and new_data > node.right.data_value:
-            return leftRotate(node)
+            return left_rotate(node)
 
         # Right Left
         if balance_factor < -1 and new_data < node.right.data_value:
-            node.right = rightRotate(node.right)
-            return leftRotate(node)
+            node.right = right_rotate(node.right)
+            return left_rotate(node)
+
+        return node
+
+    def delete(self, key):
+        self.root = self.delete_and_fix_tree(self.root, key)
+
+    def delete_and_fix_tree(self, node, key):
+        if not node:
+            return node
+        elif key < node.data_value:
+            node.left = self.delete_and_fix_tree(node.left, key)
+        elif key > node.data_value:
+            node.right = self.delete_and_fix_tree(node.right, key)
+
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+
+            node.data_value = get_min_in_sub_tree(node.right)
+            node.right = self.delete_and_fix_tree(node.right, node.data_value)
+
+        if node is None:
+            return node
+
+        node.height = 1 + max(get_height(node.left), get_height(node.right))
+
+        # Step 3 - Get the balance factor
+        balance = get_balance_factor(node)
+
+        # Left Left
+        if balance > 1 and get_balance_factor(node.left) >= 0:
+            return right_rotate(node)
+
+        #  Left Right
+        if balance > 1 and get_balance_factor(node.left) < 0:
+            node.left = left_rotate(node.left)
+            return right_rotate(node)
+
+        # Right Right
+        if balance < -1 and get_balance_factor(node.right) <= 0:
+            return left_rotate(node)
+
+        # Right Left
+        if balance < -1 and get_balance_factor(node.right) > 0:
+            node.right = right_rotate(node.right)
+            return left_rotate(node)
 
         return node
 
@@ -49,7 +97,7 @@ def get_height(node):
     return node.height
 
 
-def leftRotate (current_node):
+def left_rotate(current_node):
     right_child = current_node.right
     left_child_of_right_child = right_child.left
     right_child.left = current_node
@@ -60,7 +108,7 @@ def leftRotate (current_node):
     return right_child
 
 
-def rightRotate(current_node):
+def right_rotate(current_node):
     left_child = current_node.left
     right_child_of_left_child = left_child.right
     left_child.right = current_node
@@ -84,3 +132,9 @@ def pre_order_print(current_node):
             pre_order_print(current_node.left)
         if current_node.right is not None:
             pre_order_print(current_node.right)
+
+
+def get_min_in_sub_tree(current_node):
+    while current_node.left is not None:
+        current_node = current_node.left
+    return current_node.data_value
